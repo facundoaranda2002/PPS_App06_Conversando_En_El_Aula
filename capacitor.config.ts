1c0263ba9a7f89bd22ec4f932ac4f520050edf00
Gabriela Berasain <facundoaranda67@gmail.com>
2024-05-03 19:32:10 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ppsapp04',
+  webDir: 'www'
+};
+
+export default config;
